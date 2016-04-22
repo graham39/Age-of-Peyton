2026c5a3ee5554e3b82f4e143ffb8b6100e714f7
Graham Rosenberg <grahamrosenberg@Graham-Mac.local>
2016-04-22 13:16:26 -0600

    This is a test

--- Age of Peyton/ViewController.swift
+++ Age of Peyton/ViewController.swift
@@ -9,16 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let stringFromTextField = enterHumanYearsTextField.text!
+        let intFromString = Int(stringFromTextField)!
+        
+        dogYearsLabel.text = "\(intFromString * 7) DogYears"
+        dogYearsLabel.hidden = false
+        
+        enterHumanYearsTextField.resignFirstResponder()
+        
+        
+    }
+    @IBAction func converToRealDogYearsButtonPressed(sender: UIButton) {
+        
+        let stringFromTextField = enterHumanYearsTextField.text!
+        
+        let doubleFromTextField = Double(stringFromTextField)!
+        
+        var realDogYears:Double
+        
+        if doubleFromTextField > 2  {
+            realDogYears = (10.5 * 2) + (doubleFromTextField - 2) * 4
+        }
+        else {
+            realDogYears = doubleFromTextField * 10.5
+        }
+        
+        dogYearsLabel.hidden = false
+        
+        dogYearsLabel.text = "\(realDogYears) Real Dog Years"
+        
+        enterHumanYearsTextField.resignFirstResponder()
+        
+    }
+    
+    
+    
 
 
 }
